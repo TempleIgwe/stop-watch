434232dd77f1b09311a796074f283c75b5b3561d
TempleIgwe <temple.igw@gmail.com>
2022-11-13 18:31:28 +0100

    Add timer buttons

---
+++ main.js
@@ -0,0 +1,4 @@
+const timer = document.querySelector(".watch");
+const startBtn = document.querySelector(".start");
+const pauseBtn = document.querySelector(".pause");
+const resetBtn = document.querySelector(".reset");
